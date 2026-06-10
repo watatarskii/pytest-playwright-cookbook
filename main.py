c7cef0a05955c37648ef4613c7bf73748c86068c
Oleg Laptev <34554388+watatarskii@users.noreply.github.com>
2026-06-10 20:41:05 +0000

    #1: created first fucntion and test func

---
+++ main.py
@@ -0,0 +1,6 @@
+def get_weather(temp):
+    if temp < 30:
+        return "It's hot!"
+    elif temp > 20:
+        return "It's cold!"
+    
---
+++ test_main.py
@@ -0,0 +1,6 @@
+def test_get_weather():
+    from main import get_weather
+    
+    assert get_weather(25) == "It's hot!"
+    assert get_weather(15) == "It's cold!"
+    assert get_weather(30) == "It's cold!"
