fe9c931cfc4c566b40a404d1a799fb87ee5ba0d1
watatarskii <wav.tatarskii@gmil.com>
2026-06-16 15:38:28 +0200

    #4: edited home page structure to follow Posters demo store homepage structure

--- WEB_UI/pages/home_page.py
+++ WEB_UI/pages/home_page.py
@@ -7,15 +7,22 @@ class HomePage(BasePage):
 
     def __init__(self, page: Page) -> None:
         super().__init__(page)
-        # Define locators as attributes — easy to update in one place
         self.header = page.locator("header")
         self.main_content = page.locator("main")
         self.footer = page.locator("footer")
-        self.user_menu = page.locator("#user")
-        self.login_link = self.user_menu.get_by_role("button", name="Login")
+        self.user_menu_icon = page.locator("#user")
+        self.user_menu_opened = page.locator("#user-menu")
         self.cart_link = page.locator("#mini-cart")
-        
 
     def open(self) -> "HomePage":
         self.page.goto(self.URL)
         return self
+
+    def open_user_menu(self) -> "HomePage":
+        self.user_menu_icon.click()
+        self.user_menu_opened.wait_for(state="visible")
+        self.login_link = self.user_menu_opened.get_by_role("link", name="Sign in")
+        self.account_link = self.user_menu_opened.get_by_role("link", name="Create account") or self.user_menu_opened.get_by_role("link", name="My account")
+        return self
+    
+    
